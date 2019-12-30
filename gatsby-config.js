27f5aff499fdf3806221aef24a8fa2d92df2e07e
TrushanT <trushant.ivision@gmail.com>
2019-12-30 18:14:00 +0530

    updating favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `iVision Web Studio`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
         // banner:`src/images/ivision.jpg`
       },
     },
